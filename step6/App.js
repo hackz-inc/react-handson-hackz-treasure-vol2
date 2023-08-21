71fb2b8856feee05c64a2b909e763229e1811967
take-cantik <take.cantik17@gmail.com>
2023-08-21 14:29:56 +0900

    :sparkles: add step 6 code

---
+++ step6/App.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [isStart, setStart] = useState(false);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (isStart) {
+      const timer = setInterval(() => {
+        setCount((prevCount) => prevCount + 1);
+      }, 1000);
+
+      return () => clearInterval(timer);
+    }
+  }, [isStart]);
+
+  return (
+    <main className="App">
+      <h1>{count}</h1>
+      <button onClick={() => setStart((prev) => !prev)}>
+        {isStart ? "ストップ" : "スタート"}
+      </button>
+    </main>
+  );
+}
